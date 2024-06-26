ee4a1e1493561c6b095b31cd6b866d279c7bf3f5
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-06-26 23:49:32 +0300

    shows how to manipulate variables with the help of arithmetic calculations

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1=31;
+let num2=2;
+let num3=17;
+let num4=13;
+
+
+let multiply=num1*num2;
+
+let random=Math.ceil(Math.random());
+let mod=num3%num4;
+
+let max=Math.max(1,2,3,4,5,20);
